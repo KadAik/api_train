0bba27cdef8d57275e2da30ad3b7a012c5944c71
Kad <learning.aik@gmail.com>
2025-01-31 17:54:03 +0100

    Create models

--- LittleLemon/LocalLibrary/models.py
+++ LittleLemon/LocalLibrary/models.py
@@ -1,3 +1,118 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 
-# Create your models here.
+import uuid
+import inflect
+
+p = inflect.engine()
+
+def image_path(instance, filename):
+    """ Return the image path as a string based on the app name and the model name."""
+    app_name = instance._meta.app_label
+    model_name = instance._meta.model_name
+    return f"{app_name}/{p.plural(model_name)}/{filename}"
+
+
+
+class Genre(models.Model):
+    """Model representing a book genre : eg. Science fiction, Novel, Poetry..."""
+    
+    name = models.CharField(max_length=255, unique=True, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    
+    
+    def __str__(self):
+        """Return a string to represent the model object"""
+        
+        return self.name
+    
+    
+    def get_absolute_url(self):
+        """Return the url to access a particular genre instance details"""
+        
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+                UniqueConstraint(
+                    Lower('name'),
+                    name='genre_name_case_insensitive_unqie',
+                    violation_error_message="Genre already exists (case insensitive match)"
+                ),
+        ]
+
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("Author", on_delete=models.RESTRICT, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    ISBN = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>'
+                            )
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+    
+    
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    cover_page = models.ImageField(upload_to=image_path, blank=True, null=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.last_name}, {self.first_name}'
