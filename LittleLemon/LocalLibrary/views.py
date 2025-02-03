85f019bebb3efd069699b2b96340653c366e75c1
Kad <learning.aik@gmail.com>
2025-02-03 17:44:21 +0100

    Creating home page with some views

--- LittleLemon/LocalLibrary/views.py
+++ LittleLemon/LocalLibrary/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author
+from django.views import View
 
-# Create your views here.
+
+def index(request):
+    """ Render the home page."""
+    # Counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    num_authors = Author.objects.count()    # all() implied by default
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    
+    return render(request, 'LocalLibrary/index.html', context=context)
+
+
+
+class BookView(View):
+    """Class to handle all requests associated with books."""
+    
+    template = 'LocalLibrary/books/index.html'
+    
+    def get(self, request, *args, **kwargs):
+        books = Book.objects.all()
+        context = {'books': books}
+        
+        return render(request, BookView.template, context)
